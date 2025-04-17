11b6ad9271c40890a21fcc518bbeff73c6649674
Prashant Puri <32349676+prashantpuri112@users.noreply.github.com>
2025-04-16 22:35:54 -0500

    Update exam1prashantpuri.py

--- exam1prashantpuri.py
+++ exam1prashantpuri.py
@@ -10,7 +10,8 @@ st.title('🚗 Exam 1 - Automobile Data Analysis')
 st.markdown('Prepared by **Prashant Puri** | Banner ID: 001397936 | MS in Analytics | SLU')
 
 # Load dataset from local file
-df = pd.read_csv('CleanedAutomobile.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/klamsal/Fall2024Exam/refs/heads/main/CleanedAutomobile.csv
+')
 st.success('✅ Data loaded successfully.')
 st.dataframe(df.head())
 
